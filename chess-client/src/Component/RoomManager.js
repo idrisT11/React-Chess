01a4a32410865c5823c797f38be568bf31fe4e59
idrisT11 <64688223+idrisT11@users.noreply.github.com>
2021-10-05 21:13:37 +0100

    shabbism

--- chess-client/src/Component/RoomManager.js
+++ chess-client/src/Component/RoomManager.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Plateau from './Plateau/Plateau'
 
 export default class RoomManager extends Component {
 
@@ -15,7 +16,23 @@ export default class RoomManager extends Component {
         return (
             <div>
                 <div>
-                    <span> </span>
+                    <span>دخل محمد في الغرفة لاعبًا</span>
+                    <span>دخل كمل في الغرفة لاعبًا</span>
+                    <span>دخل طاهر في الغرفة مشاهدًا</span>
+                    <span>خرج محمد من الغرفة</span>
+
+                    <button>مستعد ؟</button>
+                </div>
+                <div>
+
+                    <Plateau />
+                </div>
+
+                <div>
+                    <span>سلام<strong> :محمد </strong> </span>
+                    <span>كي راك دير خي<strong> :طاهر </strong> </span>>
+
+                    <button>برعث</button>     
                 </div>
             </div>
         )
